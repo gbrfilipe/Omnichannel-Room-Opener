4d161dc888dc86e693ed7563a158120e6dadfe33
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2022-08-28 19:12:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 url_rc = "ROCKET_CHAT_URL" # without the end slash
 department = "DEPARTMENT"
 
+###############################################################
+
 def open_room():
 
   time.sleep(1)
@@ -18,7 +20,7 @@ def open_room():
   room_endpoint = "/api/v1/livechat/room"
   url_visitor = url_rc + visitor_endpoint
   url_room = url_rc + room_endpoint + "?token=" + token
-  ###############################################################
+  
 
   requests_random_user = requests.get(url_random_user)
   random_user = requests_random_user.json()
@@ -27,7 +29,7 @@ def open_room():
   email = random_user["results"][0]["email"]
   phone = random_user["results"][0]["phone"]
 
-  ###############################################################
+  
 
   visitor_payload = json.dumps({
     "visitor": {
@@ -43,16 +45,18 @@ def open_room():
     'Content-Type': 'application/json'
   }
 
-  ###############################################################
-
   new_visitor = requests.post(url_visitor, headers = visitor_headers, data = visitor_payload)
 
   #print(new_visitor.text)
 
-  ###############################################################
+  
 
   room = requests.get(url_room)
   print(room.json())
+  
+  
+###############################################################
+
 
 count = 1 # How many rooms?
 
