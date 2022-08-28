3e5d54c71e06dd5a0fa6bc4b72426ed401772a89
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2022-08-28 18:53:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
 import requests
 import json
 import uuid
+import time
 
-token = str(uuid.uuid4())
+url_rc = "ROCKET_CHAT_URL" # without the end slash
+department = "DEPARTMENT"
 
-url_rc = "https://ROCKET_CHAT_SERVER" # without the end slash
-department = "DEPARTMENT_NAME"
+def open_room():
 
-url_random_user = "https://randomuser.me/api/"
+  time.sleep(1)
 
-visitor_endpoint = "/api/v1/livechat/visitor"
-room_endpoint = "/api/v1/livechat/room"
-url_visitor = url_rc + visitor_endpoint
-url_room = url_rc + room_endpoint + "?token=" + token
+  token = str(uuid.uuid4())
 
-###############################################################
+  url_random_user = "https://randomuser.me/api/"
 
-requests_random_user = requests.get(url_random_user)
-random_user = requests_random_user.json()
+  visitor_endpoint = "/api/v1/livechat/visitor"
+  room_endpoint = "/api/v1/livechat/room"
+  url_visitor = url_rc + visitor_endpoint
+  url_room = url_rc + room_endpoint + "?token=" + token
+  ###############################################################
 
-name = (random_user["results"][0]["name"]["first"] + " " + random_user["results"][0]["name"]["last"])
-email = random_user["results"][0]["email"]
-phone = random_user["results"][0]["phone"]
+  requests_random_user = requests.get(url_random_user)
+  random_user = requests_random_user.json()
 
-###############################################################
+  name = (random_user["results"][0]["name"]["first"] + " " + random_user["results"][0]["name"]["last"])
+  email = random_user["results"][0]["email"]
+  phone = random_user["results"][0]["phone"]
 
-visitor_payload = json.dumps({
-  "visitor": {
-    "department": department,
-    "name": name,
-    "email": email,
-    "token": token,
-    "phone": phone
+  ###############################################################
+
+  visitor_payload = json.dumps({
+    "visitor": {
+      "department": department,
+      "name": name,
+      "email": email,
+      "token": token,
+      "phone": phone
+    }
+  })
+
+  visitor_headers = {
+    'Content-Type': 'application/json'
   }
-})
 
-visitor_headers = {
-  'Content-Type': 'application/json'
-}
+  ###############################################################
 
-###############################################################
+  new_visitor = requests.post(url_visitor, headers = visitor_headers, data = visitor_payload)
 
-new_visitor = requests.post(url_visitor, headers = visitor_headers, data = visitor_payload)
+  #print(new_visitor.text)
 
-#print(new_visitor.text)
+  ###############################################################
 
-###############################################################
+  room = requests.get(url_room)
+  print(room.json())
 
-room = requests.get(url_room)
-print(room.json())
+count = 10 # How many rooms?
+
+for i in range(count):
+  open_room()
