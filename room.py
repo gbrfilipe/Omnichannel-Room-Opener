10e79878a088a12dba7ad2d355da5e96c47a4e0f
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2022-08-30 23:38:33 -0300

    Update room.py

--- room.py
+++ room.py
@@ -16,15 +16,12 @@ def open_room(url_rc, department):
   url_visitor = url_rc + visitor_endpoint
   url_room = url_rc + room_endpoint + "?token=" + token
   
-
   requests_random_user = requests.get(url_random_user)
   random_user = requests_random_user.json()
 
   name = (random_user["results"][0]["name"]["first"] + " " + random_user["results"][0]["name"]["last"])
   email = random_user["results"][0]["email"]
   phone = random_user["results"][0]["phone"]
-
-  
 
   visitor_payload = json.dumps({
     "visitor": {
@@ -43,8 +40,6 @@ def open_room(url_rc, department):
   new_visitor = requests.post(url_visitor, headers = visitor_headers, data = visitor_payload)
 
   #print(new_visitor.text)
-
-  
 
   room = requests.get(url_room)
   print(room.json())
