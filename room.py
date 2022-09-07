750dde91e98cc120147a54f7d4ed0d488b8d03fa
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2022-09-06 22:44:27 -0300

    Update room.py

--- room.py
+++ room.py
@@ -1,45 +1,35 @@
 import requests
 import json
-import uuid
 import time
 
-def open_room(url_rc, department):
+def open_room(url_rc, department, count, name, email, phone, token):
 
-  time.sleep(1)
+  for i in range(count):
 
-  token = str(uuid.uuid4())
+    time.sleep(1)
 
-  url_random_user = "https://randomuser.me/api/"
+    visitor_endpoint = "/api/v1/livechat/visitor"
+    room_endpoint = "/api/v1/livechat/room"
+    url_visitor = url_rc + visitor_endpoint
+    url_room = url_rc + room_endpoint + "?token=" + token[i]
 
-  visitor_endpoint = "/api/v1/livechat/visitor"
-  room_endpoint = "/api/v1/livechat/room"
-  url_visitor = url_rc + visitor_endpoint
-  url_room = url_rc + room_endpoint + "?token=" + token
-  
-  requests_random_user = requests.get(url_random_user)
-  random_user = requests_random_user.json()
+    visitor_payload = json.dumps({
+      "visitor": {
+        "department": department,
+        "name": name[i],
+        "email": email[i],
+        "token": token[i],
+        "phone": phone[i]
+      }
+    })
 
-  name = (random_user["results"][0]["name"]["first"] + " " + random_user["results"][0]["name"]["last"])
-  email = random_user["results"][0]["email"]
-  phone = random_user["results"][0]["phone"]
-
-  visitor_payload = json.dumps({
-    "visitor": {
-      "department": department,
-      "name": name,
-      "email": email,
-      "token": token,
-      "phone": phone
+    visitor_headers = {
+      'Content-Type': 'application/json'
     }
-  })
 
-  visitor_headers = {
-    'Content-Type': 'application/json'
-  }
+    new_visitor = requests.post(url_visitor, headers = visitor_headers, data = visitor_payload)
 
-  new_visitor = requests.post(url_visitor, headers = visitor_headers, data = visitor_payload)
+    #print(new_visitor.text)
 
-  #print(new_visitor.text)
-
-  room = requests.get(url_room)
-  print(room.json())
+    room = requests.get(url_room)
+    print(room.json())
