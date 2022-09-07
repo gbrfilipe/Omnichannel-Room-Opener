3659947221db5bcb2c0a8fb77e2f6828b3928a40
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2022-09-06 23:09:30 -0300

    Update visitors.py

--- visitors.py
+++ visitors.py
@@ -1,16 +1,16 @@
 import requests
 import uuid
-
-count = 2
+import json
 
 name = []
 email = []
 phone = []
 token = []
 
-def visitors(count):
+def visitors(count: int, department: str, url_rc: str):
 
   url_random_user = "https://randomuser.me/api/?results=" + str(count)
+  url_visitor = url_rc + "/api/v1/livechat/visitor"
 
   requests_random_user = requests.get(url_random_user)
   random_user = requests_random_user.json()
@@ -21,4 +21,22 @@ def visitors(count):
     phone.append(random_user["results"][i]["phone"])
     token.append(str(uuid.uuid4()))
 
-  return name, email, phone, token
+    visitor_payload = json.dumps({
+      "visitor": {
+        "department": department,
+        "name": name[i],
+        "email": email[i],
+        "token": token[i],
+        "phone": phone[i]
+      }
+    })
+
+    visitor_headers = {
+      'Content-Type': 'application/json'
+    }
+
+    new_visitor = requests.post(url_visitor, headers = visitor_headers, data = visitor_payload)
+
+    #print(new_visitor.text)
+
+  return token
