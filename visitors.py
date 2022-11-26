5456e6cccf03649b1f1b34823ea24e2f9629b560
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2022-11-26 20:35:40 -0300

    update to use the faker library

--- visitors.py
+++ visitors.py
@@ -1,27 +1,29 @@
 import requests as req
+from faker import Faker
+import uuid
 import json
+
+fake = Faker()
 
 token = []
 
 def visitors(count: int, department: str, url_rc: str):
 
-  url_random_user = "https://randomuser.me/api/?results=" + str(count)
   url_visitor = url_rc + "/api/v1/livechat/visitor"
 
-  random_user = req.get(url_random_user).json()
-
   for i in range(count):
+
+    visitor_token = str(uuid.uuid4())
     
-    full_name = random_user["results"][i]["name"]["first"] + " " + random_user["results"][i]["name"]["last"]
-    token.append(random_user["results"][i]["login"]["uuid"])
+    token.append(visitor_token)
 
     new_visitor_payload = json.dumps({
       "visitor": {
         "department": department,
-        "name": full_name,
-        "email": random_user["results"][i]["email"],
-        "token": random_user["results"][i]["login"]["uuid"],
-        "phone": random_user["results"][i]["phone"]
+        "name": fake.name(),
+        "email": fake.email(),
+        "token": visitor_token,
+        "phone": fake.phone_number()
       }
     })
 
@@ -29,7 +31,7 @@ def visitors(count: int, department: str, url_rc: str):
       'Content-Type': 'application/json'
     }
 
-    new_visitor = req.post(url_visitor, headers = new_visitor_headers, data = new_visitor_payload)
+    new_visitor = req.post(url_visitor, headers = new_visitor_headers, data = new_visitor_payload, timeout=3)
 
     # print(new_visitor.text)
 
