1e6171a2784d4786149489fa45a67bf34665d75f
Gabriel Dantas <48440319+gbrfilipe@users.noreply.github.com>
2022-11-26 20:36:27 -0300

    update to send a message

--- room.py
+++ room.py
@@ -1,5 +1,6 @@
 import requests as req
 import time
+import json
 
 def open_room(count: int, url_rc: str, token: str):
 
@@ -9,5 +10,19 @@ def open_room(count: int, url_rc: str, token: str):
 
     url_room = url_rc + "/api/v1/livechat/room" + "?token=" + token[i]
 
-    room = req.get(url_room)
+    room = req.get(url_room, timeout=3)
     print(room.json())
+
+    url_message = url_rc + "/api/v1/livechat/message"
+
+    message = json.dumps({"token": token[i], "rid": room.json()["room"]["_id"], "msg": "test.."})
+
+
+    message_headers = {
+      'Content-Type': 'application/json'
+    }
+
+    time.sleep(3)
+
+    send_message = req.post(url_message, headers = message_headers, data = message, timeout=3)
+    print(send_message)
